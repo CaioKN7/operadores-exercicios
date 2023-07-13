07ff0677e626fed074e124f2d45cbea801ca7e2a
CaioKN7 <galdinocaiost@gmail.com>
2023-07-13 18:48:28 -0300

    Exercicio 1 operadores - feito

---
+++ exercicio1/script.js
@@ -0,0 +1,3 @@
+const requisicaoDeNumero = +prompt ("Por favor, insira um número par")
+console.log(requisicaoDeNumero % 2)
+//todos os numeros pares sobrarão 0, se dividido por 2. E numero impar sobrará 1 sempre.
